9be01bed1bc4bdf9b7ebaa7c0818cde70d1871f6
shanikashushan <2410511106003@live4.utcc.ac.th>
2025-09-26 11:30:52 +0700

    fix bug

--- login-app/src/config/supabase.js
+++ login-app/src/config/supabase.js
@@ -3,10 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://hbjcidsorfdjgvjwcwky.supabase.com";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhiamNpZHNvcmZkamd2andjd2t5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNTEyODYsImV4cCI6MjA3MzkyNzI4Nn0.9NrqxRfd_LolTQ-SnF6i2rrPAA44LFROTqX8ZD356cY";
 export const supabase = createClient(supabaseUrl, supabaseKey, {
- auth: {
- storage: AsyncStorage,
- autoRefreshToken: true,
- persistSession: true,
- detectSessionInUrl: false,
- },
+    auth: {
+        storage: AsyncStorage,
+        autoRefreshToken: true,
+        persistSession: true,
+        detectSessionInUrl: false,
+    },
 })
